7571b1e933bec2ab4b319d43ab742a293c2059a3
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-08 14:43:19 +0800

    refactor tests and add function

--- string_calculator.py
+++ string_calculator.py
@@ -4,44 +4,31 @@ import re
 class StringCalculator():
     def __init__(self) -> None:
         pass
-    
+
     def parse(self, number_string, delimiter=',|\n'):
         # split by comma or newline
         return re.split(delimiter, number_string)
 
     def extract_delimiter(self, input_string):
         if input_string == '':
-            return ('', '')
-        elif input_string[:2] == '//': # contains delimiter
+            return '', ''
+        elif input_string[:2] == '//':
             result = input_string.split('\n')
             delimiter = result[0][-1]
             number_string = result[1]
-            return (delimiter, number_string)
+            return delimiter, number_string
         else:
-            return ('', input_string)
-        
-        # Regex = f”//{delimiter}\n{.+}”
-        # f'([0-9{delimiter}]+)'
+            return '', input_string
 
-        # return (a, b)
+    def add(self, input_string):
+        delimiter, number_string = self.extract_delimiter(input_string)
 
-    def add(self, number_string):
         if number_string == "":
             return 0
-        
-        number_strings = self.parse(number_string)
-        numbers = [int(num) for num in number_strings]
 
-        return sum(numbers)
-
-    def calculate(self, input_string):
-        delimiter, number_string = self.extract_delimiter(input_string)
         if delimiter == '':
             number_strings = self.parse(number_string)
         else:
             number_strings = self.parse(number_string, delimiter)
-        
-        numbers = [int(num) for num in number_strings]
 
-        return sum(numbers)
-        
+        return sum([int(num) for num in number_strings])
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -2,11 +2,12 @@ import unittest
 
 from string_calculator import StringCalculator
 
+
 class StringCalculatorTest(unittest.TestCase):
     def test_should_return_zero_given_empty_string(self):
         result = StringCalculator().add("")
         self.assertEqual(0, result)
-    
+
     def test_should_return_same_number_given_string_of_single_number(self):
         result = StringCalculator().add("1")
         self.assertEqual(1, result)
@@ -28,7 +29,7 @@ class StringCalculatorTest(unittest.TestCase):
         self.assertEqual(6, result)
 
     def test_should_return_sum_given_string_of_numbers_with_new_format(self):
-        result = StringCalculator().calculate("//;\n1;2")
+        result = StringCalculator().add("//;\n1;2")
         self.assertEqual(3, result)
 
     def test_extract_delimiter_should_split_delimiter_and_number_string(self):
@@ -38,10 +39,19 @@ class StringCalculatorTest(unittest.TestCase):
     def test_extract_delimiter_should_return_no_delimiter_and_number_string_given_no_delimiter(self):
         result = StringCalculator().extract_delimiter("1,2")
         self.assertEqual(("", "1,2"), result)
-    
+
     def test_extract_delimiter_should_return_tuple_of_empty_strings_given_empty_input(self):
         result = StringCalculator().extract_delimiter("")
         self.assertEqual(("", ""), result)
 
+    def test_should_parse_default_delimiter(self):
+        result = StringCalculator().parse("1\n2,3,4\n5,66")
+        self.assertEqual(["1", "2", "3", "4", "5", "66"], result)
 
+    def test_should_parse_semicolon_delimiter(self):
+        result = StringCalculator().parse("1;2;3", ";")
+        self.assertEqual(["1", "2", "3"], result)
 
+    def test_should_return_0_if_no_number_in_input_string(self):
+        result = StringCalculator().add("//;\n")
+        self.assertEqual(0, result)
